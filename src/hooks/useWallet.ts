9701b3a504ca3c52dbc8f5bd105105e1db742b91
angel112 <angel.lakra@chainlabs.in>
2022-07-11 11:07:39 +0530

    wallet network switch vulnerability

--- src/hooks/useWallet.ts
+++ src/hooks/useWallet.ts
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import { NETWORK } from "../constants";
+import { CHAIN_ID, NETWORK } from "../constants";
 
 const useWallet = (): any => {
   const [provider, setProvider] = useState<ethers.providers.Web3Provider>();
@@ -24,10 +24,11 @@ const useWallet = (): any => {
   }, []);
 
   useEffect(() => {
+    console.log({ provider });
     if (provider && provider?.provider?.isMetaMask) {
       provider.getNetwork().then((network) => {
         const networkId = network.chainId;
-        if (networkId !== 4) {
+        if (networkId !== parseInt(CHAIN_ID)) {
           // @ts-ignore
           window.ethereum.request({
             method: "wallet_switchEthereumChain",
@@ -45,19 +46,45 @@ const useWallet = (): any => {
   }, [provider]);
 
   useEffect(() => {
-    signer?.getAddress().then((user) => {
-      setUser(user);
-    });
+    try {
+      signer?.getAddress().then((user) => {
+        setUser(user);
+      });
+    } catch (error) {
+      console.log({ error });
+    }
   }, [signer]);
 
   const connectWallet = () => {
     if (typeof window !== "undefined") {
       const getProvider = async () => {
-        const provider = await new ethers.providers.Web3Provider(
+        let provider = await new ethers.providers.Web3Provider(
           // @ts-expect-error ethereum in window is not defined
           window.ethereum,
           "any"
         );
+        provider.getNetwork().then(async (network) => {
+          const networkId = network.chainId;
+          if (networkId !== parseInt(CHAIN_ID)) {
+            // @ts-ignore
+            await window.ethereum.request({
+              method: "wallet_switchEthereumChain",
+              params: [
+                {
+                  chainId: `0x${CHAIN_ID}`,
+                },
+              ],
+            });
+
+            setProvider(
+              await new ethers.providers.Web3Provider(
+                // @ts-expect-error ethereum in window is not defined
+                window.ethereum,
+                "any"
+              )
+            );
+          }
+        });
         // @ts-expect-error ethereum in window is not defined
         window.ethereum.enable();
         setProvider(provider);
