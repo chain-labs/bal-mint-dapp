893bdb83dca015b1f17900148c99113df8e5ff77
angel112 <angel.lakra@chainlabs.in>
2022-07-15 14:50:27 +0530

    audit

--- src/containers/home.old.tsx
+++ src/containers/home.old.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
 import { TwitterFill } from "akar-icons";
 import { BigNumber, ethers } from "ethers";
